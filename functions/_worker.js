bfbf50653ff88c1211ee0cc9d021a8c7c4897fa1
pagarpanel24 <pagarpanel24@gmail.com>
2024-08-27 17:36:36 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
     console.log('Serving index.html');
     return context.env.ASSETS.fetch(context.request);
   } else {
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbz-_YfM4FuqA5eBQduKW7bujHCMF9sUXI_wrMSWBAj_KJI8K5ur_mC3zYS3l1PqJQHZvw/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbxXOicAa6Ro0s-Z3xrXp_4HJJe7UTqi8EhGH0OAZBqjpqe4HJDvbfrnX1RNr_yoptBi/exec';
     const newUrl = new URL(scriptUrl);
     newUrl.searchParams.set('path', url.pathname);
     console.log('Redirecting to:', newUrl.toString());
