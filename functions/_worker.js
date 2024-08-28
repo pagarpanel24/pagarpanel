11f95220cd053bf6e9b847505bbdc3739e190fac
pagarpanel24 <pagarpanel24@gmail.com>
2024-08-28 03:50:02 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
     console.log('Serving index.html');
     return context.env.ASSETS.fetch(context.request);
   } else {
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbxXOicAa6Ro0s-Z3xrXp_4HJJe7UTqi8EhGH0OAZBqjpqe4HJDvbfrnX1RNr_yoptBi/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbzJqbUtTRV6r0OxfJQTWnYejo7BMX9d1k1rezXbWow4sHMoMWWvadEwunv9T8TA4HO-/exec';
     const newUrl = new URL(scriptUrl);
     newUrl.searchParams.set('path', url.pathname);
     console.log('Redirecting to:', newUrl.toString());
